716a721ba38356259bd40d93fb49fbc98631ec3b
Atharv-web <aargangodkar@gmail.com>
2025-09-04 22:02:17 +0530

    made some changes

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from langchain.schema import HumanMessage
 from dotenv import load_dotenv
 load_dotenv()
 
+port = int(os.getenv("PORT", 8501))
+st.set_option("server.port", port)
+st.set_option("server.address", "0.0.0.0")
+
 @st.cache_resource
 def load_embedding_model():
     return SentenceTransformer("intfloat/multilingual-e5-large")
