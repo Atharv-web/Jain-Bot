38d654c9d1cb2d31897b2c9bb8e2224c8321a458
Atharv-web <aargangodkar@gmail.com>
2025-09-05 16:44:47 +0530

    made some changes for deploying

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from api.jain_agent import chatbot
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import os
 
 app = FastAPI()
 
@@ -13,12 +15,13 @@ app.add_middleware(
     allow_methods = ["*"],
 )
 
-app.mount("/", StaticFiles(directory="public", html=True), name="static")
-
-class Query(BaseModel):
-    query : str
+@app.get("/")
+async def read_index():
+    return FileResponse(os.path.join("public", "index.html"))
 
 @app.post("/chat")
-async def chat_endpoint(req: Query):
-    answer = await chatbot(req.query)
+async def chat_endpoint(req:Request):
+    body = await req.json()
+    user_query = body.get("query","")
+    answer = await chatbot(user_query)
     return {"answer": answer}
